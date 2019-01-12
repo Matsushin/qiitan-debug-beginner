129a3cd5b47144698eb71b6c7a2e9b393506bc32
matsushin <matsushin.dev@gmail.com>
2019-01-12 11:57:40 +0900

    メールを送れるように

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'qiitan-debug-beginner.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
